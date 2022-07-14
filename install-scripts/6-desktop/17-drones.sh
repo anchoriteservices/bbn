0bd20770bc03aa9890ed44af8111ba014f9dfee8
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2022-07-14 03:01:32 +0100

    Update 17-drones.sh

--- install-scripts/6-desktop/17-drones.sh
+++ install-scripts/6-desktop/17-drones.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 apt-get install -y libqt5quickwidgets5 libqt5webkit5 libqt5serialport5 \
- libqt5svg5 libqt5serialport5 libqt5opengl5 qml-module-qtquick-controls libsndfile1
+ libqt5svg5 libqt5serialport5 libqt5opengl5 libqt5test5 qml-module-qtquick-controls libsndfile1
 
 # https://github.com/ArduPilot/apm_planner
 wget https://github.com/bareboat-necessities/apm_planner_4rpi/releases/download/v2.0.29-rc1-16-g339533bfb/apmplanner2_2.0.29-rc1-16-g339533bfb_armhf.deb
