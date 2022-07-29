9673d945c3e4a0c4d817262298ce4de82f50c3da
mgrouch <mgrushinskiy@gmail.com>
2022-07-29 18:09:40 -0400

    parallel cmake

--- install-scripts/8-apps/files/add-ons/stdc-install.sh
+++ install-scripts/8-apps/files/add-ons/stdc-install.sh
@@ -5,7 +5,7 @@
 cd /usr/local/share
 cd inmarsatc
 sudo mkdir -p build && cd build
-sudo cmake ..
+sudo cmake -j4 ..
 sudo make -j4
 sudo make install
 sudo make clean
@@ -13,7 +13,7 @@ cd ../../
 
 cd stdcdec
 sudo mkdir -p build && cd build
-sudo cmake ..
+sudo cmake -j4 ..
 sudo make -j4
 sudo make install
 sudo make clean
