015870c2c54603356c0ed65b9cf40f373103c44c
mgrouch <mgrushinskiy@gmail.com>
2022-08-02 01:53:07 -0400

    build performance

--- install-scripts/4-server/04-signalk.sh
+++ install-scripts/4-server/04-signalk.sh
@@ -68,7 +68,7 @@ npm cache clean --force
 
 ## Install signalk published plugins
 pushd /home/signalk/.signalk
-  su signalk -c "export MAKEFLAGS='-j 8' \
+  su signalk -c "export MAKEFLAGS='-j 8'; \
                  npm install @signalk/charts-plugin  \
                  sk-resources-fs  \
                  freeboard-sk-helper  \
