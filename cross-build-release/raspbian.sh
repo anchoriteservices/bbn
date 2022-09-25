f9d7340ae20175a6a9c8975c2fb385224709e8b5
mgrouch <mgrushinskiy@gmail.com>
2022-09-25 11:06:14 -0400

    upd incremental build

--- cross-build-release/raspbian.sh
+++ cross-build-release/raspbian.sh
@@ -24,13 +24,13 @@
   myCache=./cache/$thisArch
   prefix=$myCache/$zipName
 {
-  echo curl -k -L -o $prefix.part1 $imageSource.part1
-  echo curl -k -L -o $prefix.part2 $imageSource.part2
-  echo curl -k -L -o $prefix.part3 $imageSource.part3
-  echo curl -k -L -o $prefix.part4 $imageSource.part4
-  echo curl -k -L -o $prefix.part5 $imageSource.part5
-  echo curl -k -L -o $prefix.part6 $imageSource.part6
-} | xargs -L 1 -I CMD -P 6 bash -c CMD
+  echo curl --progress-bar -k -L -o $prefix.part1 $imageSource.part1
+  echo curl --progress-bar -k -L -o $prefix.part2 $imageSource.part2
+  echo curl --progress-bar -k -L -o $prefix.part3 $imageSource.part3
+  echo curl --progress-bar -k -L -o $prefix.part4 $imageSource.part4
+  echo curl --progress-bar -k -L -o $prefix.part5 $imageSource.part5
+  echo curl --progress-bar -k -L -o $prefix.part6 $imageSource.part6
+} | xargs -I CMD -P 6 bash -c CMD
 
   cat $prefix.part? > $myCache/$zipName
   rm $prefix.part?
